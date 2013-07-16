dd009d5aa7670ed44b9de7d1aeb87af7bdace5f3
Christian Carter <cdcarter@gmail.com>
2013-07-16 16:38:37 -0400

    the great write method

--- lib/dino/components/printer.rb
+++ lib/dino/components/printer.rb
@@ -21,8 +21,10 @@ module Dino
   		# account for idle, start, and stop bits.
   		# The AdaFruit library used a setTimeout sorta situation but for now
   		# we are just going to sleep().
-
   		BYTE_TIME = (11.to_f  / BAUDRATE)
+
+  		# This amount will sleep 10 times on a wake command.
+			WAKE_INCREMENT = 0.5
 
   		attr_accessor :baud, :timeout, :dot_print_time, :dot_feed_time,
   									:prev_byte, :column, :max_column, :char_height,
@@ -32,11 +34,11 @@ module Dino
   			options[:baud] ||= BAUDRATE
   			@baud = options.baud
   			@serial = SoftwareSerial.new(board:self.board, pins: self.pins, baud: self.baud)
-  			default_state	
+  			reset_state	
   		end
 
   		# set up all the important variables for printing.
-  		def default_state
+  		def reset_state
   			@prev_byte = '\n'
   			@column = 0
   			@max_column = 32
@@ -50,16 +52,52 @@ module Dino
   		# reset the printer to its starting state as well as reset our 
   		# variables used for tracking
   		def reset
-  			default_state
+  			reset_state
   			write_bytes(27,64)
   		end
 
+  		# set the default printing parameters
+  		def default
+  			# online
+  			# justify :left
+  			# inverse :off
+  			# double_height :off
+  			# line_height = 32
+  			# bold :off
+  			# underline :off
+  			# @barcode_height = 50
+  			# size :standard
+  		end
+
+  		# this is our very low level write-a-byte method for all high-level
+  		# printing.
+  		def write(byte)
+  			if (byte != 0x13)
+  				timeout_wait
+  				write_bytes(byte)
+  				wait = BYTE_TIME
+  				if((byte == '\n') || (@column == @max_column))
+  					wait += (@prev_byte == '\n') ? ((@char_height+@line_spacing) * @dot_feed_time) :
+  																					((@char_height*@dot_print_time)+(@line_spacing*@dot_feed_time))
+  					@column = 0
+  					byte = '\n'
+  				else
+  					column += 1
+  				end
+  				@timeout = wait
+  				@prev_byte = byte
+  			end
+  		end
+
+  		# sometimes the printer goes to sleep, we want to wake it up.
+  		# the manual recommends 50ms delay, but we're gonna do 5 full sec
+  		# with no-ops spaced in between. cause...better safe than sorry
   		def wake
   			@timeout = 0
   			write_bytes(255)
   			(1..10).each do |i| 
   				write_bytes(27)
-  				@timeout = .5
+  				@timeout = WAKE_INCREMENT
   			end
   		end
 
